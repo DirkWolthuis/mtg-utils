3b58cbdd4c34e06f820ef05db50170ab1392eb0f
dirk.wolthuis <hallo@ikbendirk.nl>
2026-05-06 21:43:36 +0200

    install material

--- apps/mtg-smart-tabletop/src/app/app.ts
+++ apps/mtg-smart-tabletop/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, MatSlideToggle],
   selector: 'stt-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
